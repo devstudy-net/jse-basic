8ad2a74c38486b970cd4d91b80f97650d9756afc
Devstudy.Net <devstudy.net@gmail.com>
2016-09-18 16:50:24 +0300

    Hello world

---
+++ src/net/devstudy/basic/lection3/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.devstudy.basic.lection3;
+
+/**
+ *
+ * @author devstudy
+ * @see http://devstudy.net
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
